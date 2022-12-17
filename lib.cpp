96669df38c7f78dc2fc1943140c737f6fe08584c
LucaSorann0 <118919272+LucaSorann0@users.noreply.github.com>
2022-12-17 11:53:32 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,6 +1,6 @@
 #include "lib.h"
 
-int ver (const char lista[10][20],const char nome[20]) {
+int ver (char lista[10][20],char nome[20]) {
 for (int k=0; k<10; k++) {
 if (strcmp (lista[k], nome) == 0) {
 return k;
